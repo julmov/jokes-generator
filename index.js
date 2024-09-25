f202c990a75fd9f8afd795cbec166191b0788313
julmov <ijuliamovchan@icloud.com>
2024-09-25 13:55:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function ShowAnswer(response) {
+  //alert(response.data.answer);
+  new Typewriter("#joke", {
+    strings: `${response.data.answer}`,
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
+
+let generateJoke = () => {
+  let apiKey = "6557bae1f19ecd4eect5adbe4o7e0153";
+  let context = "be polite and provide a very short answer";
+  let prompt = "Tell me a joke about French people";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Processing");
+
+  axios.get(apiUrl).then(ShowAnswer);
+};
+
+let button = document.querySelector("button");
+button.addEventListener("click", generateJoke);
